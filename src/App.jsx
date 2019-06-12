feb1254fa5314f1687271f257431c68eb6ce75eb
unknown <juliansotoponce@gmail.com>
2019-06-12 09:31:05 -0700

    Render timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+class App extends Component {
+	state = {
+		totalTime: 0,
+		previousTime: 0,
+		isResting: false,
+		intervalID: null,
+		stopped: true,
+		pomodoroMinutes: 25,
+		restMinutes: 5
+	}
+
+	frame = () => {
+		this.setState(prevState => {
+			return {
+				totalTime: prevState.totalTime + Date.now() - prevState.previousTime,
+				previousTime: Date.now()
+			}
+		}, () => {
+			if (this.state.isResting){
+				if (this.state.totalTime/1000/60 > this.state.restMinutes){
+					this.setState({
+						isResting: false,
+						totalTime: 0
+					});
+				}
+			} else {
+				if (this.state.totalTime/1000/60 > this.state.pomodoroMinutes){
+					this.setState({
+						isResting: true,
+						totalTime: 0
+					});
+				}
+			}
+		});
+	}
+
+	start = () => {
+		if (this.state.stopped){
+			this.setState({
+				totalTime: 0,
+				previousTime: Date.now(),
+				isResting: false,
+				stopped: false,
+				intervalID: setInterval(() => {
+					this.frame();
+				}, 1000/60)
+			});
+		}
+	}
+
+	stop = () => {
+		clearInterval(this.state.intervalID);
+		this.setState({
+			stopped: true
+		});
+	}
+
+	render(){
+		const sec = Math.floor(this.state.totalTime/1000)%60,
+			min = Math.floor(this.state.totalTime/1000/60);
+		const clock = `${min}:${sec < 10 ? `0` : ``}${sec}`;
+
+		return (
+			<div>
+				<span>{ clock }</span>
+				{
+					this.state.stopped ? (
+						<button onClick={this.start}>Start</button>
+					) : (
+						<button onClick={this.stop}>Stop</button>
+					)
+				}{
+					this.state.stopped ? null : (
+						this.state.isResting ? <span>Rest!</span> : <span>Work on it!</span>
+					)
+				}
+			</div>
+		)
+	}
 }
 
 export default App;
